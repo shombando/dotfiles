f84a6e423415eceec99d518b35f167c27cd14121
Shom Bandopadhaya <shom@bandopadhaya.com>
2024-04-20 02:24:11 -0500

    add unstable channel: but alacritty has deriv conflict

--- flake.nix
+++ flake.nix
@@ -3,16 +3,18 @@
 
 	inputs = {
 		nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+		nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 		home-manager.url = "github:nix-community/home-manager/release-23.11";
 		home-manager.inputs.nixpkgs.follows = "nixpkgs";
 	};
 
-	outputs = { self, nixpkgs, home-manager, ... }:
+	outputs = { self, nixpkgs, nixpkgs-unstable, home-manager, ... }:
 	let
 		# System Settings
 		system = "x86_64-linux";
 		lib = nixpkgs.lib;
 		pkgs = nixpkgs.legacyPackages.${system};
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
 
 		# User Settings
 		username = "shom";
@@ -22,14 +24,19 @@
 		nixosConfigurations = {
 			vm = lib.nixosSystem {
 				inherit system;
-
 				modules = [ ./configuration.nix ];
+        specialArgs = {
+          inherit pkgs-unstable;
+        };
 			};
 		};
 		homeConfigurations = {
 			shom = home-manager.lib.homeManagerConfiguration {
 				inherit pkgs;
 				modules = [ ./home.nix ];
+        extraSpecialArgs = {
+          inherit pkgs-unstable;
+        };
 			};
 		};
 	};
--- home.nix
+++ home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 
 {
   # Let Home Manager install and manage itself.
