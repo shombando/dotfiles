0a34644da835d3ca46604f310a10210cbac2cb97
Shom Bandopadhaya <shom@bandopadhaya.com>
2024-04-20 16:54:55 -0500

    zsh plugins and config

--- modules/shell.nix
+++ modules/shell.nix
@@ -25,7 +25,7 @@ in {
     bash
   ];
 
-  programs.bash = {
+ programs.bash = {
     enable = true;
     shellAliases = shAliases;
   };
@@ -33,46 +33,41 @@ in {
   # ZSH is available system wide, but I want home.nix to be portable
 	programs.zsh = {
 		enable = true;
+    dotDir = ".config/zsh";
     shellAliases = shAliases;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
     # autosuggestions.enable = true;
     # historySubstringSearch = true;
-	};
 
-  programs.zsh.plugins = [
-    {
-      # will source zsh-autosuggestions.plugin.zsh
-      name = "zsh-autosuggestions";
-      src = pkgs.fetchFromGitHub {
-        owner = "zsh-users";
-        repo = "zsh-autosuggestions";
-        rev = "v0.7.0";
-        # sha256 = lib.fakeSha256;
-        sha256 = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w";
-      };
-    }
-    {
-      # will source zsh-autosuggestions.plugin.zsh
-      name = "zsh-history-substring-search";
-      src = pkgs.fetchFromGitHub {
-        owner = "zsh-users";
-        repo = "zsh-history-substring-search";
-        rev = "v1.1.0";
-        # sha256 = lib.fakeSha256;
-        sha256 = "sha256-GSEvgvgWi1rrsgikTzDXokHTROoyPRlU0FVpAoEmXG4";
-      };
-    }
-    {
-      # will source zsh-autosuggestions.plugin.zsh
-      name = "spaceship-prompt";
-      src = pkgs.fetchFromGitHub {
-        owner = "spaceship-prompt";
-        repo = "spaceship-prompt";
-        rev = "v4.15.1";
-        # sha256 = lib.fakeSha256;
-        sha256 = "sha256-5nlku/JUs1YnchOGvsle7rF96b3DteMuxZhSzoNvDGY";
-      };
-    }
-  ];
+    initExtra = ''
+bindkey '^[[A' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+'';
+
+    plugins = [
+      {
+        # will source zsh-autosuggestions.plugin.zsh
+        name = "zsh-autosuggestions";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-autosuggestions";
+          rev = "v0.7.0";
+          # sha256 = lib.fakeSha256;
+          sha256 = "sha256-KLUYpUu4DHRumQZ3w59m9aTW6TBKMCXl2UcKi4uMd7w";
+        };
+      }
+      {
+        # will source zsh-history-substring-search.plugin.zsh
+        name = "zsh-history-substring-search";
+        src = pkgs.fetchFromGitHub {
+          owner = "zsh-users";
+          repo = "zsh-history-substring-search";
+          rev = "v1.1.0";
+          # sha256 = lib.fakeSha256;
+          sha256 = "sha256-GSEvgvgWi1rrsgikTzDXokHTROoyPRlU0FVpAoEmXG4";
+        };
+      }
+    ];
+  };
 }
